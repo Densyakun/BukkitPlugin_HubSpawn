f3ba8c7393242368d466a08952f71b9e88f48a22
電車君 <Densyakun1128@gmail.com>
2016-01-13 01:03:24 +0900

    no message

--- src/org/densyakun/csvm/ColumnNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.densyakun.csvm;
-public class ColumnNotFoundException extends Throwable{
-	private static final long serialVersionUID=4159600689386640315L;
-	public ColumnNotFoundException(int l,int c){
-		super("Line:"+l+" Column:"+c);
-	}
-}
--- src/org/densyakun/csvm/LineNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.densyakun.csvm;
-public class LineNotFoundException extends Throwable{
-	private static final long serialVersionUID=-68296175430166153L;
-	public LineNotFoundException(int l){
-		super("Line:"+l);
-	}
-}
